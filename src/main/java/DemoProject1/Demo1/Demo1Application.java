aed86380d770cdc5836bef678cb3be52ec670f03
Mohamed Ibrahim <m.ibrahim.mustafa@gmail.com>
2021-12-18 14:42:49 +0000

    add demo file

---
+++ src/main/java/DemoProject1/Demo1/Demo1Application.java
@@ -0,0 +1,13 @@
+package DemoProject1.Demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/DemoProject1/Demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package DemoProject1.Demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
